7692db6d2c383c208ef1676fcbf669988a2c7640
guru1889 <mguru1889@gmail.com>
2026-03-14 13:29:57 +0530

    fix media

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -110,6 +110,8 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # MEDIA FILES
 import os
